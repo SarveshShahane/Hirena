41d726eb5fa5b2bf7934a26270642bf2e5873e21
Om Shahane <71377782+shahane806@users.noreply.github.com>
2025-10-16 22:14:09 +0530

    Add comment in admin.js for clarity

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,7 @@ const Job = require("../models/job");
 const Application = require("../models/application");
 const categories = require("../init/categories");
 const User = require("../models/user");
+// om shahane
 module.exports.dashboard=async (req, res) => {
     const jobs = await Job.find({}).populate("applications");
     res.render("admin/dashboard.ejs", {
